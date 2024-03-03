23ae52997edcb6b618055ae522f24775c7f54922
leon-dream1~ <nahidulislam019045@gmail.com>
2024-03-03 16:23:52 +0600

    All post shown in post container done

---
+++ js/index.js
@@ -0,0 +1,75 @@
+const loadAllPost = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/posts"
+  );
+  const data = await res.json();
+
+  const postContainer = document.getElementById("post-container");
+  data.posts.forEach((post) => {
+    console.log(post);
+    const div = document.createElement("div");
+    div.id = post.id;
+    div.innerHTML = `<div
+    class="post flex flex-row mb-5 bg-[#F3F3F5] rounded-[24px] p-[40px] gap-[24px]"
+  >
+    <div class="relative">
+      <img
+        class="h-[72px] w-[72px] bg-white rounded-2xl"
+        src=${post.image}
+        alt=""
+      />
+      <div class="${post.isActive ? 'bg-[#10B981]': 'bg-[#FF3434]'} rounded-full w-[20px] h-[20px] absolute top-[-5px] right-[-5px]"></div>
+    </div>
+    <div class="w-[100%] space-y-4">
+      <div class="flex gap-8">
+        <h4
+          class="text-[#12132DCC] text-[14px] font-inter font-medium"
+        >
+          #${post.category}
+        </h4>
+        <h4
+          class="text-[#12132DCC] text-[14px] font-inter font-medium"
+        >
+          Author : ${post.author.name}
+        </h4>
+      </div>
+      <h2 class="text-[#12132D] text-[20px] font-bold">
+        ${post.title}
+      </h2>
+      <p
+        class=" text-[#12132D99] text-[16px] font-inter font-normal border-b border-dashed border-[#12132D40] pb-4"
+      >
+        ${post.description}
+      </p>
+      <div class="flex justify-between pt-4">
+        <div class="icon flex space-x-16">
+          <div>
+            <div class="flex space-x-4">
+              <img class="" src="./images/comment.png" alt="" />
+            <span class="text-[16px] text-[#12132D99] font-normal font-inter">${post.comment_count}</span>
+            </div>
+          </div>
+          <div>
+            <div class="flex space-x-4">
+              <img src="./images/eye.png" alt="" />
+              <span class="text-[16px] text-[#12132D99] font-normal font-inter"> ${post.view_count}</span>
+            </div>
+          </div>
+          <div>
+            <div class="flex space-x-4">
+              <img src="./images/clock.png" alt="" />
+              <span class="text-[16px] text-[#12132D99] font-normal font-inter">${post.posted_time}</span>
+            </div>
+          </div>
+        </div>
+        <div class="">
+          <img src="./images/email.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+    postContainer.appendChild(div);
+  });
+};
+loadAllPost();
