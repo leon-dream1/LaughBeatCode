4e0f9fea690b56199c5c6f1548ac9e9e613c9bb5
leon-dream1~ <nahidulislam019045@gmail.com>
2024-03-04 02:50:14 +0600

    Post and latest post section responsive design added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-const loadAllPost = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/retro-forum/posts"
-  );
+const loadAllPost = async (category) => {
+  let res;
+  const spinner = document.getElementById("post_spinner");
+  if (category) {
+    document.getElementById("post-container").innerHTML = "";
+    spinner.classList.remove("hidden");
+    res = await fetch(
+      `https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`
+    );
+  } else {
+    res = await fetch(
+      "https://openapi.programming-hero.com/api/retro-forum/posts"
+    );
+  }
+
   const data = await res.json();
+  // console.log(data.posts);
 
   const postContainer = document.getElementById("post-container");
   data.posts.forEach((post) => {
@@ -10,20 +22,20 @@ const loadAllPost = async () => {
     const div = document.createElement("div");
     div.id = post.id;
     div.innerHTML = `<div
-    class="post flex flex-row mb-5 bg-[#F3F3F5] rounded-[24px] p-[40px] gap-[24px]"
+    class="post flex flex-col lg:flex-row mb-5 bg-[#F3F3F5] rounded-[24px] p-[15px] lg:p-[40px] lg:gap-[24px]"
   >
     <div class="relative">
       <img
-        class="h-[72px] w-[72px] bg-white rounded-2xl"
+        class="h-[72px] w-[72px] mx-auto lg:mx-0 bg-white rounded-2xl"
         src=${post.image}
         alt=""
       />
       <div class="${
         post.isActive ? "bg-[#10B981]" : "bg-[#FF3434]"
-      } rounded-full w-[20px] h-[20px] absolute top-[-5px] right-[-5px]"></div>
+      } rounded-full w-[20px] h-[20px] absolute top-[-4px] md:top-[-4px] lg:top-[-5px] right-[39%] md:right-[44%] lg:right-[-5px]"></div>
     </div>
     <div class="w-[100%] space-y-4">
-      <div class="flex gap-8">
+      <div class="flex flex-col text-center lg:text-start lg:flex-row gap-3 lg:gap-8">
         <h4
           class="text-[#12132DCC] text-[14px] font-inter font-medium"
         >
@@ -35,54 +47,53 @@ const loadAllPost = async () => {
           Author : ${post.author.name}
         </h4>
       </div>
-      <h2 class="text-[#12132D] text-[20px] font-bold">
+      <h2 class="text-[#12132D] text-center lg:text-start text-[16px] lg:text-[20px] font-bold">
         ${post.title}
       </h2>
       <p
-        class=" text-[#12132D99] text-[16px] font-inter font-normal border-b border-dashed border-[#12132D40] pb-4"
+        class=" text-[#12132D99] text-[16px] text-center lg:text-start font-inter font-normal border-b border-dashed border-[#12132D40] pb-4"
       >
         ${post.description}
       </p>
-      <div class="flex justify-between pt-4">
-        <div class="icon flex space-x-16">
+      <div class="flex flex-col lg:flex-row lg:justify-between pt-4">
+        <div class="icon flex flex-col lg:flex-row space-y-3 lg:space-y-0 lg:space-x-16">
           <div>
-            <div class="flex space-x-4">
-              <img class="" src="./images/comment.png" alt="" />
-            <span class="text-[16px] text-[#12132D99] font-normal font-inter">${
+            <div class="flex flex-col lg:flex-row space-y-3 lg:space-y-0 lg:space-x-4">
+              <img class="w-[20%] lg:w-[100%] mx-auto lg:mx-0" src="./images/comment.png" alt="" />
+            <span class="text-[16px] text-[#12132D99] font-normal font-inter text-center lg:text-start">${
               post.comment_count
             }</span>
             </div>
           </div>
           <div>
-            <div class="flex space-x-4">
-              <img src="./images/eye.png" alt="" />
-              <span class="text-[16px] text-[#12132D99] font-normal font-inter"> ${
+            <div class="flex flex-col lg:flex-row space-y-3 lg:space-y-0 lg:space-x-4">
+              <img class="w-[20%] lg:w-[100%] mx-auto lg:mx-0" src="./images/eye.png" alt="" />
+              <span class="text-[16px] text-[#12132D99] font-normal font-inter text-center lg:text-start"> ${
                 post.view_count
               }</span>
             </div>
           </div>
           <div>
-            <div class="flex space-x-4">
-              <img src="./images/clock.png" alt="" />
-              <span class="text-[16px] text-[#12132D99] font-normal font-inter">${
+            <div class="flex flex-col lg:flex-row space-y-3 lg:space-y-0 lg:space-x-4">
+              <img class="w-[20%] lg:w-[100%] mx-auto lg:mx-0" src="./images/clock.png" alt="" />
+              <span class="text-[16px] text-[#12132D99] font-normal font-inter text-center lg:text-start">${
                 post.posted_time
               }</span>
             </div>
           </div>
         </div>
-        <div class="">
+        <div class='mt-[30px]'>
           <img onclick="addReadPost(${post.id}, '${post.title.replace(
       /'/g,
       "@"
     )}', ${
       post.view_count
-    })" class='cursor-pointer' src="./images/email.png" alt="" />
+    })" class='cursor-pointer w-[20%] lg:w-[100%] mx-auto' src="./images/email.png" alt="" />
         </div>
       </div>
     </div>
   </div>`;
 
-    const spinner = document.getElementById("post_spinner");
     setTimeout(() => {
       spinner.classList.add("hidden");
     }, 2000);
@@ -94,14 +105,21 @@ const loadAllPost = async () => {
 };
 loadAllPost();
 
+const searchedPost = () => {
+  const searchedPost = document.getElementById("search-post").value;
+  // console.log(searchedPost);
+  loadAllPost(searchedPost);
+  document.getElementById("search-post").value = "";
+};
+
 const addReadPost = (postId, postTitle, postViewCount) => {
-  console.log(postId, postTitle.replace("@", `'`), postViewCount);
+  // console.log(postId, postTitle.replace("@", `'`), postViewCount);
   const markAsReadContainer = document.getElementById("mark-as-read-container");
   const div = document.createElement("div");
-  div.innerHTML = `<div class="bg-white rounded-[16px] p-[15px]">
-  <div class="flex justify-between">
-    <h4>${postTitle.replace("@", `'`)}</h4>
-    <div class="flex gap-4">
+  div.innerHTML = `<div class="bg-white rounded-[16px] p-[10px] lg:p-[15px]">
+  <div class="flex flex-row items-center justify-between">
+    <h4 class='text-[14px]'>${postTitle.replace("@", `'`)}</h4>
+    <div class="flex flex-row gap-1 lg:gap-4">
       <img
         class="w-[28px] h-[28px]"
         src="./images/eye.png"
@@ -124,7 +142,7 @@ const loadLatestPost = async () => {
     "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
   );
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
 
   const latestPostContainer = document.getElementById("latest-post-container");
   data.forEach((latestPost) => {
